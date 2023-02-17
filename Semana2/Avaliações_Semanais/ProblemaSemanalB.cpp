97201d0f300096604d8135dd3b93089773d97723
Sequeira12 <brunomsequeira@hotmail.com>
2023-02-17 17:05:01 +0000

    Semanal feito

--- Semana2/Avaliações_Semanais/ProblemaSemanalB.cpp
+++ Semana2/Avaliações_Semanais/ProblemaSemanalB.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
+#include <unordered_set>
+
+
 using namespace std;
+
+unordered_set<int> P;
+int chessGame(int x,int y, int m);
 int main(){
+    int valorFinal = 0;
     int n_cavalos;
     cin >> n_cavalos;
-    cout << n_cavalos << endl;
+    
     for(int i = 0; i < n_cavalos; i++){
         int pos1,pos2,move;
         cin >> pos1 >> pos2 >> move;
-        cout << pos1 << " "<< pos2 << " "<< move << endl;
+        pos1+=200;
+        pos2+=200;
+        valorFinal += chessGame(pos1,pos2,move);
     }
+    cout << valorFinal << endl;
     return 0;
+}
+
+
+int chessGame(int x,int y, int m){
+    
+    int z = (x + y)*(x + y + 1)/2 + y;
+    //cout << z << endl;
+    if(m == 0){
+        if(P.count(z)>0){
+            return 0;
+        }else{
+            P.insert(z);
+            return 1;
+        }
+    }else{
+        int v = 0;
+        if(P.count(z)==0){
+         
+            P.insert(z);
+            v = 1;
+        }
+            return (v + chessGame(x+2,y+1,m-1) + 
+        chessGame(x+2,y-1,m-1) + chessGame(x-2,y+1,m-1) + chessGame(x-2,y-1,m-1)+
+        chessGame(x-1,y+2,m-1) + chessGame(x-1,y-2,m-1) +chessGame(x+1,y+2,m-1)+chessGame(x+1,y-2,m-1) );
+        
+    }
 }
