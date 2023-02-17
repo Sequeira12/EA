ca11def8d5a24594830ba7d801aff06d08acad08
Sequeira12 <brunomsequeira@hotmail.com>
2023-02-17 10:57:55 +0000

    inicio B

--- Semana2/Avaliações_Semanais/ProblemaSemanalB.cpp
+++ Semana2/Avaliações_Semanais/ProblemaSemanalB.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-    
+    int n_cavalos;
+    cin >> n_cavalos;
+    cout << n_cavalos << endl;
+    for(int i = 0; i < n_cavalos; i++){
+        int pos1,pos2,move;
+        cin >> pos1 >> pos2 >> move;
+        cout << pos1 << " "<< pos2 << " "<< move << endl;
+    }
+    return 0;
 }
