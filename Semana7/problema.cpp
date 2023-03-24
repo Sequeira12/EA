384da4c8b13e4f956a376a1e2018ae166d555cca
Sequeira12 <brunomsequeira@hotmail.com>
2023-03-24 10:45:29 +0000

    Semana 7 Completa

--- Semana7/problema.cpp
+++ Semana7/problema.cpp
@@ -4,50 +4,90 @@
 #include <vector>
 using namespace std;
 
-int casos, N, M;
+int n, m, p1, p2;
 int valorReal = 0;
 int help;
 int k;
-int F(vector<vector<bool>> &dp, int soma, int tam, vector<int> &valores);
+int N;
+int best = 0;
+vector<vector<bool>> g;
+
+int ub(int i, vector<bool> &x, int contador)
+{
+    for (int j = i; j <= N - 1; j++)
+    {
+
+        bool b = true;
+        for (int k = 0; k <= i - 1; k++)
+        {
+            if (x[k] == true and g[j][k] == true)
+            {
+                b = false;
+                break;
+            }
+        }
+        if (b == true)
+        {
+
+            contador += 1;
+        }
+    }
+    return contador;
+}
+
+void F(int i, vector<bool> &x, int contador)
+{
+
+    if (i == N)
+    {
+        if (contador > best)
+        {
+            best = contador;
+        }
+        return;
+    }
+    if (ub(i, x, contador) <= best)
+    {
+        return;
+    }
+
+    x[i] = false;
+    F(i + 1, x, contador);
+    bool b = true;
+    for (int j = 0; j <= i - 1; j++)
+    {
+
+        if (x[j] == true and g[i][j] == true)
+        {
+            b = false;
+            return;
+        }
+    }
+    if (b == true)
+    {
+        x[i] = true;
+
+        F(i + 1, x, contador + 1);
+    }
+}
+
 int main()
 {
-    // n m k
 
-    while (cin >> casos)
+    cin >> N >> m;
+
+    vector<bool> x = vector<bool>(N, false);
+    g = vector<vector<bool>>(N, vector<bool>(N, false));
+    while (m > 0)
     {
-        vector<int> position;
 
-        for (int i = 0; i < casos; i++)
-        {
-            cin >> N >> M;
-            vector<int> position(N, 0);
-            for (int k = 0; k < N; k++)
-            {
-                cin >> help;
-                position[k] = help;
-            }
-            vector<bool> positionProtected(N, false);
-            sort(position.begin(), position.end());
-            int contadorGuardas = 0;
-            for (int l = 0; l < position.size(); l++)
-            {
-                if (positionProtected[l] == false)
-                {
-
-                    positionProtected[l] = true;
-                    int valor = position[l] + 2 * M;
-                    for (int a = l; a < position.size(); a++)
-                    {
-                        if (position[a] <= valor)
-                        {
-                            positionProtected[a] = true;
-                        }
-                    }
-
-                    contadorGuardas++;
-                }
-            }
-            cout << contadorGuardas << endl;
-        };
+        cin >> p1 >> p2;
+        g[p1][p2] = true;
+        g[p2][p1] = true;
+        m--;
     }
+
+    F(0, x, 0);
+
+    cout << best << endl;
 }
