d3252e47f91c9e8de8522d090824588f3d8c74ac
Sequeira12 <brunomsequeira@hotmail.com>
2023-04-26 12:02:58 +0100

    semana 10 completa

--- Semana10/problema.cpp
+++ Semana10/problema.cpp
@@ -1,60 +1,92 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
+#include <stack>
 
 using namespace std;
 
-typedef pair<int, int> par;
 vector<vector<int>> G;
+vector<int> dfs;
+vector<int> low;
+vector<int> parent;
+vector<bool> visited;
+vector<bool> articulation_points;
+int timee = 0;
 
-const int infinito = INT_MAX;
-
-int dijkstra(int n, int dest, vector<vector<int>> &G)
+void AP(int v)
 {
-    vector<int> dist(n, infinito);
-    priority_queue<par, vector<par>, greater<par>> pq;
-
-    dist[0] = 0;
-    pq.push({0, 0});
-
-    while (!pq.empty())
+    int children = 0;
+    visited[v] = true;
+    dfs[v] = low[v] = ++timee;
+    for (int w = 1; w < G.size(); w++)
     {
-        int u = pq.top().second;
-        pq.pop();
-
-        for (int v = 0; v < n; v++)
+        if (G[v][w])
         {
-            int w = G[u][v];
-            if (w >= 0 && dist[v] > dist[u] + w)
+            if (!visited[w])
             {
-                dist[v] = dist[u] + w;
-                pq.push({dist[v], v});
+                children++;
+                parent[w] = v;
+                AP(w);
+                low[v] = min(low[v], low[w]);
+                if (parent[v] == 0 && children > 1)
+                {
+                    articulation_points[v] = true;
+                }
+                if (parent[v] != 0 && low[w] >= dfs[v])
+                {
+                    articulation_points[v] = true;
+                }
+            }
+            else if (w != parent[v])
+            {
+                low[v] = min(low[v], dfs[w]);
             }
         }
     }
-
-    return dist[dest];
 }
 
 int main()
 {
-    int number, ids;
-    cin >> number >> ids;
-    vector<vector<int>> G(number, vector<int>(number));
-
-    for (int i = 0; i < number; i++)
+    int n;
+    while (cin >> n && n != 0)
     {
-        int id;
-        cin >> id;
-        for (int j = 0; j < number; j++)
-        {
-            cin >> G[i][j];
-        }
-    }
+        G = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
+        dfs = vector<int>(n + 1, 0);
+        low = vector<int>(n + 1, 0);
+        parent = vector<int>(n + 1, 0);
+        visited = vector<bool>(n + 1, false);
+        articulation_points = vector<bool>(n + 1, false);
+        timee = 0;
 
-    int custo = dijkstra(number, ids - 1, G);
-    cout << custo << endl;
+        int v, w;
+        while (cin >> v && v != 0)
+        {
+            while (cin.get() != '\n')
+            {
+                cin >> w;
+                G[v][w] = 1;
+                G[w][v] = 1;
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (!visited[i])
+            {
+                AP(i);
+            }
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (articulation_points[i])
+            {
+                count++;
+            }
+        }
+
+        cout << count << endl;
+    }
 
     return 0;
 }
